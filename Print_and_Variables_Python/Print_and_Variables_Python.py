01d8e5f57361328834cff8f03dae39d47b11c485
TaisaKiarkhi <tayisiyakiarkhi@gmail.com>
2023-09-06 15:28:28 +0200

    Add project files.

---
+++ Print_and_Variables_Python/Print_and_Variables_Python.py
@@ -0,0 +1,14 @@
+char_name = "John"
+age = "34"
+float_value = float(12.34)
+character_ = 'd'
+double_value = 43.2
+is_male = True
+
+print(char_name + " is his name. He is " + str(age) + " old.") 
+print("Float value: " + str(float_value))
+print("Double value: " + str (double_value))
+print("Character: "+ str(character_))
+print("Is he male?: " + str(is_male))
+
+
